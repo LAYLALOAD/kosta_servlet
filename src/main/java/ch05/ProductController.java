536dc532a4f1f00604753f0024d7461e19adcd88
LAYLALOAD <yjkim7885@knou.ac.kr>
2023-10-20 16:59:57 +0900

    231020

--- src/main/java/ch05/ProductController.java
+++ src/main/java/ch05/ProductController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/pcontrol")
+//@WebServlet("/pcontrol")
 public class ProductController extends HttpServlet {
   ProductService service;
   public ProductController() {
